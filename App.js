2b7eb128e9c40e7858a212e67f505a67e272c4ac
Anuj Kumar Nath <anujkumarnath@gmail.com>
2023-07-01 23:25:16 +0530

    Using react as a package now and using parcel to bundle and server our application

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // We want to create the following nested HTML structure using react:
 // <div id="parent">
 //   <div id="child1">
