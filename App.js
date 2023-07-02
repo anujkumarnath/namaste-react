5c501d3e5c9fd50be262e496ddb213cdd2be2b0e
Anuj Kumar Nath <anujkumarnath@gmail.com>
2023-07-02 12:58:48 +0530

    writing multiline JSX

--- App.js
+++ App.js
@@ -4,6 +4,10 @@ import ReactDOM from "react-dom/client";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // Creating React element using JSX
-const heading = <h1 id="heading" className="heading" tabIndex={5}>Namaste React 🚀</h1>;
+const heading = (
+  <h1 id="heading" className="heading" tabIndex={5}>
+    Namaste React 🚀
+  </h1>
+);
 
 root.render(heading);
